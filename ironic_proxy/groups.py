33a46bc156f3db706fcda0c6078fb9c0c1369ca8
Dmitry Tantsur <divius.inside@gmail.com>
2018-10-26 17:22:04 +0200

    Cache known nodes

--- ironic_proxy/groups.py
+++ ironic_proxy/groups.py
@@ -20,6 +20,7 @@ from ironic_proxy import conf
 
 LOG = log.getLogger(__name__)
 _POOL = None
+_CACHE = None
 
 
 def _imap_unordered(func, items):
@@ -29,26 +30,80 @@ def _imap_unordered(func, items):
     return _POOL.imap_unordered(func, items)
 
 
-def create_node(node):
-    group = node.get('conductor_group', '')
+def _source(group):
     try:
-        cli = conf.groups()[group]
+        return conf.groups()[group]
     except IndexError:
         raise common.Error('No conductors in group {group}',
                            group=group or '<default>')
+
+
+def _find_node(node_id):
+    global _CACHE
+    if _CACHE is None:
+        _CACHE = {}
+
+    node = None
+    try:
+        # Check if we already know where the node is
+        group = _CACHE[node_id]
+    except KeyError:
+        # Node unknown, let's find it
+        LOG.debug('Polling all sources to find node %s', node_id)
+        for node, group in _imap_unordered(
+                lambda args: (args[1].find_node(node_id), args[0]),
+                conf.groups().items()):
+            if node is None:
+                continue
+
+            LOG.info('Node %s found in group %s',
+                     node_id, group or '<default>')
+            # Remember where the node is located
+            _CACHE[node['uuid']] = group
+            break
+    else:
+        # Node is known, just fetch it
+        cli = _source(group)
+        node = cli.find_node(node_id)
+
+    if node is None:
+        raise common.NotFound('Node {node} was not found', node=node_id)
+    return node, group
+
+
+def _cache_nodes(nodes, group):
+    global _CACHE
+    if _CACHE is None:
+        _CACHE = {}
+
+    for node in nodes:
+        if 'uuid' in node:
+            LOG.info('Node %s found in group %s',
+                     node['uuid'], group or '<default>')
+            _CACHE[node['uuid']] = group
+
+
+def create_node(node):
+    group = node.get('conductor_group', '')
+    cli = _source(group)
     return cli.create_node(node)
 
 
 def get_node(node_id):
-    for result in _imap_unordered(lambda cli: cli.find_node(node_id),
-                                  conf.groups().values()):
-        if result is not None:
-            return result
+    return _find_node(node_id)[0]
 
 
 def list_nodes():
     result = []
     for group, cli in conf.groups().items():
         LOG.debug('Loading nodes from %s', group or '<default>')
-        result.extend(cli.list_nodes())
+        nodes = cli.list_nodes()
+        _cache_nodes(nodes, group)
+        result.extend(nodes)
     return result
+
+
+def delete_node(node_id):
+    group = _find_node(node_id)[1]
+    cli = _source(group)
+    cli.delete_node(node_id)
